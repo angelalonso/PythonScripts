1bf47938bff32a8eecc42c03dc204c8cc8ae9547
aaf <alonsofonseca.angel@gmail.com>
2017-11-12 09:53:38 +0100

    jail not working yet

--- ShellScripts/jail.sh
+++ ShellScripts/jail.sh
@@ -1,26 +1,66 @@
 #!/usr/bin/env bash
 
 CURRDIR=$(pwd)
-DIR="${CURRDIR}/chroot"
+DIR="${CURRDIR}/test"
 
-mkdir -p ${DIR}
-mkdir -p ${DIR}/etc
-mkdir -p ${DIR}/bin
 
-cd ${DIR}
 
-umount ${DIR}/proc/
-umount ${DIR}/sys/
-umount ${DIR}/dev/
 
-mount -t proc proc ${DIR}/proc/
-mount --rbind /sys ${DIR}/sys/
-mount --rbind /dev ${DIR}/dev/
 
-#cp -r /usr ${DIR}/usr
-#cp -r /lib ${DIR}/lib
-#cp /bin/bash ${DIR}/bin/bash
+dirs_prep() {
+  echo "Preparing Directories"
+  mkdir -p ${DIR}
+  mkdir -p ${DIR}/etc
+  mkdir -p ${DIR}/bin
+  echo "All good"
 
-cp /etc/resolv.conf ${DIR}/etc/resolv.conf
+}
+
+debootstrap() {
+#TODO: 
+# Workaround to: 
+#   E: Cannot install into target '/home/aaf/Software/Dev/Scripts/ShellScripts/debian' mounted with noexec or nodev
+#     (maybe not using debootstrap at all)
+# test debootstrap is installed
+# choose distro (parameter?)
+# https://wiki.ubuntu.com/DebootstrapChroot
+# https://www.lucas-nussbaum.net/blog/?p=385
+  echo "Installing system"
+
+}
+
+mounts_prep() {
+  cd ${DIR}
+
+  umount ${DIR}/proc/
+  umount ${DIR}/sys/
+  umount ${DIR}/dev/
+
+  mount -t proc proc ${DIR}/proc/
+  mount --rbind /sys ${DIR}/sys/
+  mount --rbind /dev ${DIR}/dev/
+}
+
+files_prep() {
+  rsync -avzh /usr ${DIR}
+  rsync -avzh /lib ${DIR}
+  rsync -avzh /bin ${DIR}
+
+  cp /etc/resolv.conf ${DIR}/etc/resolv.conf
+}
+
+network() {
+#TODO: 
+# https://unix.stackexchange.com/questions/98808/how-to-assign-an-additional-ip-hostname-to-a-chrooted-environment
+# add it's own network interface
+  echo "Preparing its own IP"
+
+}
+
+dirs_prep
+debootstrap
+mounts_prep
+files_prep
+network
 
 chroot ${DIR} /bin/bash
