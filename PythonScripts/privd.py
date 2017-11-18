dc2424481d27c50378a01ed9968c5eb24ed64378
aaf <alonsofonseca.angel@gmail.com>
2017-11-18 23:10:18 +0100

    checking files fine

--- PythonScripts/privd.py
+++ PythonScripts/privd.py
@@ -2,11 +2,20 @@ import os
 import subprocess
 import glob
 
-# The folder where you store your keys for this password
-# TODO: remove .sysangel
+# TODO:
+#  - Sync .ssh, .aws, .kube?
+
+# The folder where you store your keys for this password. TODO: remove .sysangel
 FOLDRKEYS = os.environ['HOME'] + "/.privd"
+# The mapping of encrypted folders and mountpoints
+#            encrypted folder: mounted folder
+CRYFS_MAP = {
+            '$HOME/Dropbox/.enc_a': os.environ['HOME'] + '/Private',
+            '$HOME/Dropbox/.enc_b': os.environ['HOME'] + '/.privd/Private.bck'
+}
 
 def mount_all(cryfs_map):
+    not_mounted = []
     # Get your password
     (pass_encfs, pass_err) = bash('/usr/bin/openssl rsautl -inkey ' + FOLDRKEYS + '/priv.key -decrypt < ' + FOLDRKEYS + '/cryfs.pass')
 
@@ -14,10 +23,11 @@ def mount_all(cryfs_map):
         dec_folder = cryfs_map[encrypted_folder].replace('$HOME', os.environ['HOME'])
 
         if not os.path.ismount(dec_folder):
+            not_mounted.append(dec_folder)
             enc_folder = encrypted_folder.replace('$HOME', os.environ['HOME'])
-            print(enc_folder)
-            print(dec_folder)
             (mount_cryfs, mount_err) = bash('echo "' + pass_encfs + '" | cryfs ' + enc_folder + ' ' + dec_folder)
+    return not_mounted
+
 
 def bash(command):
 
@@ -42,22 +52,19 @@ def check_files(folder):
 def test_mounts(cryfs_map):
     not_mounted = []
     wrong_mounted = []
+    wrong_files = {}
 
-    mount_all(cryfs_map)
+    not_mounted = mount_all(cryfs_map)
 
     # Get mounted volumes
     for encrypted_folder in cryfs_map:
         enc_folder = cryfs_map[encrypted_folder].replace('$HOME', os.environ['HOME'])
         error_files = check_files(enc_folder)
+        wrong_files[enc_folder] = error_files
 
-    return "DONE"
+    return (not_mounted, wrong_files)
 
 if __name__ == "__main__":
     # TODO: Check you are on unix-like
     # TODO: Test on Macos
-    # encrypted folder: mounted folder
-    CRYFS_MAP = {
-            '$HOME/Dropbox/.enc_a': '$HOME/Private',
-            '$HOME/Dropbox/.enc_b': '$HOME/Private.bck'
-           }
-    print(test_mounts(CRYFS_MAP))
+    (not_mounted, wrong_files) = test_mounts(CRYFS_MAP)
