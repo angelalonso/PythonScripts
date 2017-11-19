4529297745bd727f0b75f558970119c9cef68dfc
aaf <alonsofonseca.angel@gmail.com>
2017-11-19 08:29:22 +0100

    params ready

--- PythonScripts/privd.py
+++ PythonScripts/privd.py
@@ -1,8 +1,9 @@
-import os
-import sys
-import subprocess
+import argparse
 import glob
 import logging as log
+import os
+import subprocess
+import sys
 
 # TODO:
 #  - Sync .ssh, .aws, .kube?
@@ -15,6 +16,7 @@ CRYFS_MAP = {
             '$HOME/Dropbox/.enc_a': os.environ['HOME'] + '/Private',
             '$HOME/Dropbox/.enc_b': os.environ['HOME'] + '/.privd/Private.bck'
 }
+
 
 def mount_all(cryfs_map):
     not_mounted = []
@@ -29,6 +31,20 @@ def mount_all(cryfs_map):
             enc_folder = encrypted_folder.replace('$HOME', os.environ['HOME'])
             (mount_cryfs, mount_err) = bash('echo "' + pass_encfs + '" | cryfs ' + enc_folder + ' ' + dec_folder)
     return not_mounted
+
+
+def unmount_all(cryfs_map):
+    mounted = []
+
+    for encrypted_folder in cryfs_map:
+        dec_folder = cryfs_map[encrypted_folder].replace('$HOME', os.environ['HOME'])
+
+        if os.path.ismount(dec_folder):
+            mounted.append(dec_folder)
+            (unmount_cryfs, unmount_err) = bash('fusermount -u "' + dec_folder + '"')
+
+    # TODO:manage error sending
+    return mounted
 
 
 def bash(command):
@@ -52,29 +68,49 @@ def check_files(folder):
     
 
 def test_mounts(cryfs_map):
-    not_mounted = []
-    wrong_mounted = []
     wrong_files = {}
-
-    not_mounted = mount_all(cryfs_map)
 
     # Get mounted volumes
     for encrypted_folder in cryfs_map:
-        enc_folder = cryfs_map[encrypted_folder].replace('$HOME', os.environ['HOME'])
-        error_files = check_files(enc_folder)
-        wrong_files[enc_folder] = error_files
+        dec_folder = cryfs_map[encrypted_folder].replace('$HOME', os.environ['HOME'])
+        if os.path.ismount(dec_folder):
+            error_files = check_files(dec_folder)
+            wrong_files[dec_folder] = error_files
 
-    return (not_mounted, wrong_files)
+    return wrong_files
 
 if __name__ == "__main__":
     # TODO: Check you are on unix-like
     # TODO: Test on Macos
-    # TODO: use argparse https://docs.python.org/3/library/argparse.html
-    try: args = sys.argv[1]
-    except: args = ''
-    if args:
+    parser = argparse.ArgumentParser()
+    # Verbosity
+    parser.add_argument('-m', '--mount', help='Mount all defined volumes', required=False, action='store_true')
+    parser.add_argument('-u', '--umount', help='Unmount all defined volumes', required=False, action='store_true')
+    parser.add_argument('-c', '--check', help='Check integrity of all defined volumes', required=False, action='store_true')
+    parser.add_argument('-v', '--verbose', help='Show higher level of verbosity', required=False, action='store_true')
+
+    args = vars(parser.parse_args())
+
+    if args['verbose']:
         log.basicConfig(format="%(levelname)s: %(message)s", level=log.DEBUG)
     else:
-        log.basicConfig(format="%(levelname)s: %(message)s", level=log.INFO)
-    (not_mounted, wrong_files) = test_mounts(CRYFS_MAP)
-    log.debug(not_mounted)
+        log.basicConfig(format="%(levelname)s: %(message)s", level=log.ERROR)
+
+    if args['umount']:
+        if args['mount']:
+           log.error(' You have defined mounting and unmounting! \n If you want to do both, please, do one step at a time') 
+           sys.exit(2)
+        mounted = unmount_all(CRYFS_MAP)
+        log.debug(mounted)
+    elif args['mount']:
+        not_mounted = mount_all(CRYFS_MAP)
+        log.debug(not_mounted)
+    elif args['check']:
+        wrong_files = test_mounts(CRYFS_MAP)
+        print wrong_files
+        # By Default, we mount everything and check it
+    else:
+        not_mounted = mount_all(CRYFS_MAP)
+        wrong_files = test_mounts(CRYFS_MAP)
+        log.debug(not_mounted)
+        log.error(wrong_files)
