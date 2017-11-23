03e253e1dc23b4aea0695b1a72ddbf4bb160d42d
Angel Alonso <a.fonseca@foodpanda.com>
2017-11-23 11:57:41 +0100

    script for stg and prod

--- ShellScripts/aws_s3_sync.sh
+++ ShellScripts/aws_s3_sync.sh
@@ -2,15 +2,37 @@
 
 countries=(ci dz gh ke ma ng rw sn tz ug tn)
 
+stg() {
 # STAGING
-AWSACCOUNT="xxxxxxxx"
-SOURCE_FLDR="xxxxxxxxxxx"
-DEST_FLDR="yyyyyyyyyyy"
+AWSACCOUNT="xxxx-st"
+SOURCE_FLDR="live-yyy-staging.xxxxyyy.io"
+DEST_FLDR="xxxx-xxxx-yyy-stag"
+PATH_FLDR="yyy/xxxxxxxx/staging"
 
 
 for cc in ${countries[@]}; do
   echo "##### SYNCING $cc"
   AWS_PROFILE="$AWSACCOUNT" \
-    aws s3 sync s3://$SOURCE_FLDR/path/$cc  s3://$DEST_FLDR/path/$cc
+    aws s3 sync s3://$SOURCE_FLDR/${PATH_FLDR}/$cc  s3://$DEST_FLDR/${PATH_FLDR}/$cc
 done
+}
 
+prod(){
+# PRODUCTION
+AWSACCOUNT="xxxx"
+SOURCE_FLDR="live-yyy-production.xxxxyyy.io"
+DEST_FLDR="xxxx-xxxx-yyy-prod"
+PATH_FLDR="yyy/xxxxxxxx/production"
+
+
+for cc in ${countries[@]}; do
+  echo "##### SYNCING $cc"
+  AWS_PROFILE="$AWSACCOUNT" \
+    aws s3 sync s3://$SOURCE_FLDR/${PATH_FLDR}/$cc  s3://$DEST_FLDR/${PATH_FLDR}/$cc
+done
+}
+
+
+# RUN!
+stg
+prod
