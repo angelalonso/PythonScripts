f78baf4229cc6dddb67b730dd6fd6831acb01b17
aaf <alonsofonseca.angel@gmail.com>
2018-05-05 22:17:54 +0200

    adding variables on jq

--- ShellScripts/get_asg_from_tag.sh
+++ ShellScripts/get_asg_from_tag.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-PROF="europe"
 FLEET="front"
+
+while [[ "$AWS_PROF" == "" ]]; do
+  echo "No AWS PROFILE is defined"
+  read -r -p "Please enter your AWS Profile:" AWS_PROF
+done
+export AWS_PROFILE=$AWS_PROF
 #https://stackoverflow.com/questions/40027395/passing-bash-variable-to-jq-select
-AWS_PROFILE=$PROF aws autoscaling describe-auto-scaling-groups | jq '.[][] | select (.Tags[].Value=="vendorbackend")' | jq .AutoScalingGroupName
+#aws autoscaling describe-auto-scaling-groups | jq '.[][] | select (.Tags[].Value=="vendorbackend")' | jq .AutoScalingGroupName
+aws autoscaling describe-auto-scaling-groups | jq -r --arg FLEET "$FLEET" '.[][] | select (.Tags[].Value==$FLEET)' | jq .AutoScalingGroupName
