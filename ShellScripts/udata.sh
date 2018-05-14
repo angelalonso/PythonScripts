6b87bbd6b06c30e3342510cca16cb8c6d8d88b3b
Angel Alonso <a.fonseca@foodpanda.com>
2018-05-14 14:53:30 +0200

    not those ones

--- ShellScripts/udata.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "#### Running initialisation script from S3"
-wget -N https://s3-eu-west-1.amazonaws.com/userdata-legacy-europe/default-puppetless.sh -O /tmp/default-puppetless.sh
-bash -x /tmp/default-puppetless.sh
