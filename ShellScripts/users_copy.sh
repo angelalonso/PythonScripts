981d5a5c751ce7c901486bb26d65b9b3cee6e922
Angel Alonso <a.fonseca@foodpanda.com>
2018-05-22 14:20:41 +0200

    script to copy over users

--- ShellScripts/users_copy.sh
+++ ShellScripts/users_copy.sh
@@ -1,11 +1,21 @@
 #!/usr/local/env bash
 
-SERVER1=$1
-SERVER2=$2
 
-for user in $(ssh $SERVER1 "ls /home/")
-do
-  echo $user
-#  ssh $SERVER2 "useradd -m -d /home/$user $user"
-  scp $SERVER1:/home/$user/.ssh/authorized_keys ./authorized_keys_$user
+
+for server in $(cat /etc/hosts | grep whatever | awk '{print $2}' | sort | uniq); do
+  echo $server
+  for USER in user1 user2 ; do
+    echo
+    echo
+    echo "##### USER $USER in $server #####"
+    echo "Copying keys"
+    sudo cp /home/$USER/.ssh/authorized_keys $HOME/keys_$USER && sudo chown afonseca. $HOME/keys_$USER
+    ## ATTENTION: MODIFY THIS!
+    scp $HOME/keys_$USER afonseca@$server:/home/afonseca/keys_$USER
+    ssh $server "sudo useradd -m -s /bin/bash $USER; sudo mkdir /home/$USER/.ssh; sudo mv /home/afonseca/keys_$USER /home/$USER/.ssh/authorized_keys; sudo chmod 600 /home/$USER/.ssh/authorized_keys; sudo chown -R $USER. /home/$USER; sudo vigr"
+    #ssh $server "sudo useradd -m -s /bin/bash $USER; sudo mkdir /home/$USER/.ssh; sudo mv /home/afonseca/keys_$USER /home/$USER/.ssh/authorized_keys; sudo chmod 600 /home/$USER/.ssh/authorized_keys; sudo chown -R $USER. /home/$USER; sudo usermod -a -G admin $USER"
+
+    rm $HOME/keys_$USER
+  done
+
 done
