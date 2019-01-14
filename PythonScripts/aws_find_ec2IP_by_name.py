5a17afab57a2ca4a526920f54f6cf6a71f9c4f7f
Ángel Alonso Fonseca <angel.fonseca@.deliveryhero.com>
2019-01-14 11:39:52 +0100

    umount autocommit 20190114_113808

--- PythonScripts/aws_find_ec2IP_by_name.py
+++ PythonScripts/aws_find_ec2IP_by_name.py
@@ -3,8 +3,8 @@
 import boto3
 
 REGION = "eu-central-1"
-VPC = "vpc-xxxxxx"
-SEARCHED = "yyyyyyy"
+VPC = "vpc-53e22d3a"
+SEARCHED = "euvolo-"
 
 ec2 = boto3.resource('ec2', region_name=REGION)
 vpc = ec2.Vpc(VPC)
@@ -12,6 +12,6 @@ vpc = ec2.Vpc(VPC)
 for i in vpc.instances.all():
     for tag in i.tags:
         if tag['Key'] == 'Name' and SEARCHED in tag['Value']:
-          #print(tag['Value'])
+          print(tag['Value'] + " " + i.id)
           #print(i.public_ip_address)
           print(i.private_ip_address)
