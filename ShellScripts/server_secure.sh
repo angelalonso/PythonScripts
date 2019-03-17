e669d930279405927212c98f5d5f05830dae7831
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2019-03-17 08:06:02 +0100

    prepare ssh

--- ShellScripts/server_secure.sh
+++ ShellScripts/server_secure.sh
@@ -8,6 +8,11 @@ update_system() {
 }
 #  create new user, give it admin access, add public key
 add_user() {
+  echo 
+  echo "Before we start, have a public SSH key ready at your local machine"
+  echo '" you can generate an SSH keypair with: ssh-keygen -f filename -t rsa -b 4096 -C "your_email@example.com"'
+  promptValue "Press Enter to continue"
+
   promptValue "Enter your user name"
   USER=$val
   useradd -m -s /bin/bash $USER 
