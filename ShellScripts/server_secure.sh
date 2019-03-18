27e7812229d6d4696a92e8b717d95e354c0f77c0
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2019-03-18 15:45:31 +0100

    correcting backup and typo

--- ShellScripts/server_secure.sh
+++ ShellScripts/server_secure.sh
@@ -35,15 +35,15 @@ add_user() {
 #  avoid SSH as root
 #  avoid SSH with password
 ssh_tweak() {
-  # TODO: test this
+  sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.orig
   promptValue "Enter your desired SSH PORT"
   SSHPORT=$val
-  sudo sed -i .orig 's/#Port 22/Port '$SSHPORT'/g' /etc/ssh/sshd_config
+  sudo sed -i 's/#Port 22/Port '$SSHPORT'/g' /etc/ssh/sshd_config
   # https://www.cyberciti.biz/faq/how-to-disable-ssh-password-login-on-linux/
-  sudo sed -i .bak 's/#PermitRootLogin .*/PermitRootLogin no/g' /etc/ssh/sshd_config
-  sudo sed -i .bak 's/#ChallengeResponseAuthentication .*/ChallengeResponseAuthentication no/g' /etc/ssh/sshd_config
-  sudo sed -i .bak 's/#PasswordAuthentication yes/PasswordAuthenticationno/g' /etc/ssh/sshd_config
-  sudo sed -i .bak 's/UsePAM yes/UsePAM no/g' /etc/ssh/sshd_config
+  sudo sed -i 's/#PermitRootLogin .*/PermitRootLogin no/g' /etc/ssh/sshd_config
+  sudo sed -i 's/#ChallengeResponseAuthentication .*/ChallengeResponseAuthentication no/g' /etc/ssh/sshd_config
+  sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
+  sudo sed -i 's/UsePAM yes/UsePAM no/g' /etc/ssh/sshd_config
   #sudo systemctl restart sshd
 }
 #  generic function to ask for user interaction
